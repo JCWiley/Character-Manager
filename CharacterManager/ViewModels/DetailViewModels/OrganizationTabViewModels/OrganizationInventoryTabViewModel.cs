02e2e175d83a2b4eaa4f70f83f53cdd6e2a352c3
JCWiley <signupjw@gmail.com>
2021-01-28 15:41:49 -0800

    Added organization inventory tab

---
+++ CharacterManager/ViewModels/DetailViewModels/OrganizationTabViewModels/OrganizationInventoryTabViewModel.cs
@@ -0,0 +1,69 @@
+﻿using CharacterManager.Events;
+using CharacterManager.Model.Entities;
+using CharacterManager.Model.Providers;
+using Prism.Events;
+using Prism.Mvvm;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterManager.ViewModels.DetailViewModels.OrganizationTabViewModels
+{
+    public class OrganizationInventoryTabViewModel : BindableBase
+    {
+        public OrganizationInventoryTabViewModel(IEntityProvider entityProvider, IEventAggregator eventAggregator, IRegionManager regionManager)
+        {
+            EA = eventAggregator;
+            RM = regionManager;
+            EP = entityProvider;
+
+            EA.GetEvent<UIUpdateRequestEvent>().Subscribe(UIUpdateRequestExecute);
+        }
+
+        #region Variables
+        private IEventAggregator EA;
+        private IRegionManager RM;
+        private IEntityProvider EP;
+        #endregion
+
+        #region Binding Targets
+        public Organization Org
+        {
+            get
+            {
+                if (EP.CurrentTargetAsCharacter != null)
+                {
+                    return (Organization)EP.CurrentTargetAsOrganization.Item;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+        #endregion
+
+        #region Event Handlers
+        private void UIUpdateRequestExecute(ChangeType type)
+        {
+            switch (type)
+            {
+                case ChangeType.SelectedCharacterChanged:
+                    RaisePropertyChanged("Org");
+                    break;
+                case ChangeType.SelectedOrganizationChanged:
+                    break;
+                case ChangeType.JobEventListChanged:
+                    break;
+                case ChangeType.JobListChanged:
+                    break;
+                default:
+                    break;
+            }
+        }
+        #endregion
+    }
+}
