866b4244ca795aa23872a84f65f5ceb580d59c70
JCWiley <signupjw@gmail.com>
2021-02-24 15:19:11 -0800

    Creation of initial test project.

---
+++ CharacterManagerTests/Model/Providers/EntityProviderTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CharacterManager.Model.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterManager.Model.Providers.Tests
+{
+    [TestClass()]
+    public class EntityProviderTests
+    {
+        [TestMethod()]
+        public void EntityProvider_()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void NotifySelectedCharacterChanged_()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void NotifySelectedOrganizationChanged_()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddEntity_()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddChild_()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void HeadEntities_()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllChildren_()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetImmidiateChildren_()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTreeMemberForEntity_()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTreeMemberForGuid_()
+        {
+            Assert.Fail();
+        }
+    }
+}
